135ce52f8f388073690f5c7524e4d05ab363a24a
Kamil Gałuszka <galuszkak@gmail.com>
2014-06-25 12:58:28 +0200

    test for parse_date from werkezug repository (suggestion by @jezdez)

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -9,12 +9,12 @@ from djlibcloud.storage import parse_date
 class TestHelpers(unittest.TestCase):
 
     def test_parse_date(self):
+        assert parse_date('Sun, 06 Nov 1994 08:49:37 GMT    ') == datetime(1994, 11, 6, 8, 49, 37)
+        assert parse_date('Sunday, 06-Nov-94 08:49:37 GMT') == datetime(1994, 11, 6, 8, 49, 37)
+        assert parse_date(' Sun Nov  6 08:49:37 1994') == datetime(1994, 11, 6, 8, 49, 37)
+        assert parse_date('foo') is None
 
-        date = parse_date("Sunday, 06-Nov-94 08:49:37 GMT")
-        self.assertEqual(date.day, 6)
-        self.assertEqual(date.month, 11)
-        self.assertEqual(date.year, 1994)
-
-        # second case <= 68
-        date = parse_date("Sunday, 06-Nov-05 08:19:32 GMT")
-        self.assertEqual(date.year, 2005)
+    def test_parse_date_overflows(self):
+        assert parse_date(' Sun 02 Feb 1343 08:49:37 GMT') == datetime(1343, 2, 2, 8, 49, 37)
+        assert parse_date('Thu, 01 Jan 1970 00:00:00 GMT') == datetime(1970, 1, 1, 0, 0)
+        assert parse_date('Thu, 33 Jan 1970 00:00:00 GMT') is None
